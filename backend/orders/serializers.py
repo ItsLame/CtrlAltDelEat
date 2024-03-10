88d66e67bc19cf029a8d888e5328be1635b1ea0d
Janhavi Jain <janhavi.j.98@gmail.com>
2024-03-10 22:59:07 +1100

    add/view order api

---
+++ backend/orders/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from orders.models import Order
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = ['tableNumber', 'itemName', 'cost', 'status', 'alterations', 'timestamp']
+        ordering = ('timestamp')
